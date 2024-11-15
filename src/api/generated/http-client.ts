c35f27de4938f7c54578aa17377b0a4e5535d65a
wjdsbs <wjdsbss@gmail.com>
2024-11-15 18:10:59 +0900

    fix: 수정

--- src/api/generated/http-client.ts
+++ src/api/generated/http-client.ts
@@ -72,7 +72,7 @@ export class HttpClient<SecurityDataType = unknown> {
   }: ApiConfig<SecurityDataType> = {}) {
     this.instance = axios.create({
       ...axiosConfig,
-      baseURL: axiosConfig.baseURL || "https://seamlessup.com",
+      baseURL: "https://seamlessup.com",
     });
     this.secure = secure;
     this.format = format;
